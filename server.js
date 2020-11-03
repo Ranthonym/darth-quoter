f172af1c04d700f6e28e51c85122e4e617af024a
mithrax <rahul.anthonym@gmail.com>
2020-11-02 19:31:20 -0500

    convert mongo client connection to promises

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ const connectionString =
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then((client) => {
     console.log("Connected to Database");
+    const db = client.db("darth-quoter");
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get("/", (req, res) => {
+      // res.send("Hello World");
+      res.sendFile(`${__dirname}/index.html`);
+    });
+
+    app.post("/quotes", (req, res) => {
+      console.log(req.body);
+    });
   })
   .catch((error) => console.error(error));
-
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  // res.send("Hello World");
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
