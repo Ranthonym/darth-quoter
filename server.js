accab04beb86f7fdd8b918287c7824c47b25bd27
mithrax <rahul.anthonym@gmail.com>
2020-11-02 19:17:12 -0500

    setup mongo client and connect to mongodb database

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
+
+console.log("May Node be with you");
+
+const connectionString =
+  "mongodb+srv://jedi:knightacademy@cluster0.zjj8r.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+  })
+  .catch((error) => console.error(error));
+
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  // res.send("Hello World");
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
+});
